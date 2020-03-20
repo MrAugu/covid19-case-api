159d3cc28f83ea9d630a4d1edac7832e8ee7a536
MrAugu <mraugu@yahoo.com>
2020-03-20 20:29:40 +0200

    States Endpoint

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const cheerio = require("cheerio");
 
 const cache = {
     "all": {},
-    "countries": []
+    "countries": [],
+    "states": []
 };
 
 const updateAll = async () => {
@@ -140,12 +141,104 @@ const updateCountries = async () => {
   cache["countries"] = result;
 };
 
+const updateStates = async () => {
+  let response;
+  try {
+    response = await axios.get("https://www.worldometers.info/coronavirus/country/us/");
+    if (response.status !== 200) {
+      console.log("Error", response.status);
+    }
+  } catch (err) {
+      console.log(err)
+    return null;
+  }
+  const result = [];
+  const html = cheerio.load(response.data);
+  const statesTable = html("table#usa_table_countries_today");
+  const tablecells = statesTable
+    .children("tbody")
+    .children("tr")
+    .children("td");
+  const totalColumns = 7;
+  const stateColIndex = 0;
+  const casesColIndex = 1;
+  const todayCasesColIndex = 2;
+  const deathsColIndex = 3;
+  const todayDeathsColIndex = 4;
+  const curedColIndex = 5;
+  const activeColIndex = 6;
+  for (let i = 0; i < tablecells.length - totalColumns; i += 1) {
+    const cell = tablecells[i];
+
+    if (i % totalColumns === stateColIndex) {
+      let state =
+        cell.children[0].data ||
+        cell.children[0].children[0].data ||
+        cell.children[0].children[0].children[0].data ||
+        cell.children[0].children[0].children[0].children[0].data ||
+        "";
+      state = state.trim();
+      if (state.length === 0) {
+        state = cell.children[0].next.children[0].data || "";
+      }
+      result.push({ state: state.trim() || "" });
+    }
+    if (i % totalColumns === casesColIndex) {
+      let cases = cell.children.length != 0 ? cell.children[0].data : "";
+      result[result.length - 1].cases = parseInt(
+        cases.trim().replace(/,/g, "") || "0",
+        10
+      );
+    }
+    if (i % totalColumns === todayCasesColIndex) {
+      let cases = cell.children.length != 0 ? cell.children[0].data : "";
+      result[result.length - 1].todayCases = parseInt(
+        cases.trim().replace(/,/g, "") || "0",
+        10
+      );
+    }
+    if (i % totalColumns === deathsColIndex) {
+      let deaths = cell.children.length != 0 ? cell.children[0].data : "";
+      result[result.length - 1].deaths = parseInt(
+        deaths.trim().replace(/,/g, "") || "0",
+        10
+      );
+    }
+    if (i % totalColumns === todayDeathsColIndex) {
+      let deaths = cell.children.length != 0 ? cell.children[0].data : "";
+      result[result.length - 1].todayDeaths = parseInt(
+        deaths.trim().replace(/,/g, "") || "0",
+        10
+      );
+    }
+    if (i % totalColumns === curedColIndex) {
+      let cured = cell.children.length != 0 ? cell.children[0].data : "";
+      result[result.length - 1].recovered = parseInt(
+        cured.trim().replace(/,/g, "") || 0,
+        10
+      );
+    }
+    if (i % totalColumns === activeColIndex) {
+      let cured = cell.children.length != 0 ? cell.children[0].data : "";
+      result[result.length - 1].active = parseInt(
+        cured.trim().replace(/,/g, "") || 0,
+        10
+      );
+    }
+  }
+
+  cache["states"] = result;
+  console.log("Updated states.");
+};
+
 updateAll();
 updateCountries();
+updateStates();
 
 setInterval(() => {
   updateAll();
   updateCountries();
+  updateStates();
 }, 600000)
 
 app.get("/api/all/", async (req, res) => {
@@ -156,6 +249,11 @@ app.get("/api/all/", async (req, res) => {
 app.get("/api/countries/", async (req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.send(JSON.stringify(cache["countries"], null, 4));
+});
+
+app.get("/api/states/", async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify(cache["states"], null, 4));
 });
 
 app.listen(9669, function() {
