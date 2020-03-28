b8b963f20073a7cbc33aa506acaab2ff9422acbf
MrAugu <mraugu@yahoo.com>
2020-03-28 14:28:08 +0200

    API Update & States Fix

--- index.js
+++ index.js
@@ -75,6 +75,8 @@ const updateCountries = async () => {
   const criticalColIndex = 7;
   const casesPerOneMillionColIndex = 8;
   const deathsPerOneMillionColIndex = 9;
+  const firstCaseColIndex = 10;
+
   // minus totalColumns to skip last row, which is total
   for (let i = 0; i < countriesTableCells.length - totalColumns; i += 1) {
       const cell = countriesTableCells[i];
@@ -169,6 +171,10 @@ const updateCountries = async () => {
               deathsPerOneMillion.trim().replace(/,/g, "") || "0"
           );
       }
+      if (i % totalColumns === firstCaseColIndex) {
+        let firstCaseDate = cell.children.length != 0 ? cell.children[0].data : "";
+        result[result.length - 1].firstCaseDate = firstCaseDate.trim();
+      }
   }
 
   console.log("Updated The Countries. (" + (result.length) + ")");
@@ -183,82 +189,38 @@ const updateStates = async () => {
       console.log("Error", response.status);
     }
   } catch (err) {
-      console.log(err)
+    console.log(err)
     return null;
   }
+  // to store parsed data
   const result = [];
+  // get HTML and parse death rates
   const html = cheerio.load(response.data);
   const statesTable = html("table#usa_table_countries_today");
-  const tablecells = statesTable
+  const tableRows = statesTable
     .children("tbody")
-    .children("tr")
-    .children("td");
-  const totalColumns = 7;
-  const stateColIndex = 0;
-  const casesColIndex = 1;
-  const todayCasesColIndex = 2;
-  const deathsColIndex = 3;
-  const todayDeathsColIndex = 4;
-  const curedColIndex = 5;
-  const activeColIndex = 6;
-  for (let i = 0; i < tablecells.length - totalColumns; i += 1) {
-    const cell = tablecells[i];
+    .children("tr:not(.total_row)").toArray();
+  // NOTE: this will change when table format change in website
 
-    if (i % totalColumns === stateColIndex) {
-      let state =
-        cell.children[0].data ||
-        cell.children[0].children[0].data ||
-        cell.children[0].children[0].children[0].data ||
-        cell.children[0].children[0].children[0].children[0].data ||
-        "";
-      state = state.trim();
-      if (state.length === 0) {
-        state = cell.children[0].next.children[0].data || "";
-      }
-      result.push({ state: state.trim() || "" });
-    }
-    if (i % totalColumns === casesColIndex) {
-      let cases = cell.children.length != 0 ? cell.children[0].data : "";
-      result[result.length - 1].cases = parseInt(
-        cases.trim().replace(/,/g, "") || "0",
-        10
-      );
-    }
-    if (i % totalColumns === todayCasesColIndex) {
-      let cases = cell.children.length != 0 ? cell.children[0].data : "";
-      result[result.length - 1].todayCases = parseInt(
-        cases.trim().replace(/,/g, "") || "0",
-        10
-      );
-    }
-    if (i % totalColumns === deathsColIndex) {
-      let deaths = cell.children.length != 0 ? cell.children[0].data : "";
-      result[result.length - 1].deaths = parseInt(
-        deaths.trim().replace(/,/g, "") || "0",
-        10
-      );
-    }
-    if (i % totalColumns === todayDeathsColIndex) {
-      let deaths = cell.children.length != 0 ? cell.children[0].data : "";
-      result[result.length - 1].todayDeaths = parseInt(
-        deaths.trim().replace(/,/g, "") || "0",
-        10
-      );
-    }
-    if (i % totalColumns === curedColIndex) {
-      let cured = cell.children.length != 0 ? cell.children[0].data : "";
-      result[result.length - 1].recovered = parseInt(
-        cured.trim().replace(/,/g, "") || 0,
-        10
-      );
-    }
-    if (i % totalColumns === activeColIndex) {
-      let cured = cell.children.length != 0 ? cell.children[0].data : "";
-      result[result.length - 1].active = parseInt(
-        cured.trim().replace(/,/g, "") || 0,
-        10
-      );
-    }
+  const stateColIndex = 0;
+  const dataColIndexes = {
+    cases: 1,
+    todayCases: 2,
+    deaths: 3,
+    todayDeaths: 4,
+    active: 5
+  };
+
+  for (const row of tableRows) {
+
+    const cells = row.children.filter(c => c.name === "td");
+
+    const stateData = { state: parseStateCell(cells[stateColIndex]) };
+
+    for (const property in dataColIndexes)
+      stateData[property] = parseNumberCell(cells[dataColIndexes[property]]);
+
+    result.push(stateData);
   }
 
   cache["states"] = result;
@@ -293,3 +255,24 @@ app.get("/api/states/", async (req, res) => {
 app.listen(9669, function() {
   console.log("Listening on: 9669");
 });
+
+function parseStateCell(cell) {
+  let state =
+    cell.children[0].data ||
+    cell.children[0].children[0].data ||
+    // state name with link has another level
+    cell.children[0].children[0].children[0].data ||
+    cell.children[0].children[0].children[0].children[0].data ||
+    "";
+  state = state.trim();
+  if (state.length === 0) {
+    // parse with hyperlink
+    state = cell.children[0].next.children[0].data || "";
+  }
+  return state.trim() || "";
+}
+
+function parseNumberCell (cell) {
+  const cellValue = cell.children.length != 0 ? cell.children[0].data : "";
+  return parseFloat(cellValue.replace(/[,\+\-\s]/g, ""), 10) || 0;
+}
